cf122e91ca4da5982b51fbf2b91380502d1b730f
antonellamichetti <68030606+antonellamichetti@users.noreply.github.com>
2020-08-05 22:18:55 -0300

    Add files via upload

--- dos.js
+++ dos.js
@@ -10,7 +10,34 @@ Al finalizar, se debe informar:
 Considerar que las categorías pueden ser: almacen, lácteos, limpieza o bebidas y que el precio no puede ser menor a 0 ni mayor a 1000.
 */
 
-function mostrar()
-{
-	alert("Hola Mundo");
+
+
+function mostrar() {
+	let articulo;
+	let categoria;
+	let precio;
+	let respuesta = "s";
+	let contbebidas = 0;
+	let flaglacteo = 0;
+	let preciomaxlacteos = 0;
+
+	do {
+		articulo = prompt("Ingrese el nombre del artículo");
+		categoria = prompt("Ingrese la categoría del artículo");
+		precio = prompt("Ingrese el precio del artículo");
+
+		respuesta = prompt("Desea continuar ingresando productos? s/n");
+	} while (respuesta == "s");
+
+	switch (categoria) {
+		case "lacteos":
+			if (flaglacteo == 0 || precio > preciomaxlacteos) {
+				preciomaxlacteos = precio;
+				flaglacteo = 1;
+		}
+			break;
+		case "bebidas":
+			contbebidas = contbebidas + 1;
+	}
+alert (" cantidad de art. bebidas: " + contbebidas + "lacteo con mayor precio:" + preciomaxlacteos + )
 }
